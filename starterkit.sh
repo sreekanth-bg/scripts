5b6c0bd810f58b6ee2a8f68c6461342a6eacec2c
Sreekanth Govindaiah <31440235+sreekanth-bg@users.noreply.github.com>
2019-09-22 16:36:42 +0530

    Update starterkit.sh

--- starterkit.sh
+++ starterkit.sh
@@ -1,13 +1,3 @@
-################################ Setup ZSH and Oh-my-zsh on Linux 
-#Z shell or zsh is an interactive UNIX shell and a command line interpreter for scripting languages
-#Oh-my-zsh is an open source framework for managing ZSH, the Z shell
-apt install zsh
-change the default shell of the root user to zsh 
-chsh -s /usr/bin/zsh root
-which zsh
-chsh -s /usr/bin/zsh root
-echo $SHELL
-sudo apt-get update && sudo apt-get dist-upgrade && apt-get -f install
-apt install wget git
-download and execute installer
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+https://gitachub.com/robbyrussell/oh-my-zsh
+https://www.howtoforge.com/tutorial/how-to-setup-zsh-and-oh-my-zsh-on-linux/
+https://medium.com/wearetheledger/oh-my-zsh-made-for-cli-lovers-installation-guide-3131ca5491fb
